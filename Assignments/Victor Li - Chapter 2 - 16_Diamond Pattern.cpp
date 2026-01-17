5a2a0fff9bb29f73e37a961e762ef5271e508734
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-17 10:29:13 -0800

    Create Victor Li - Chapter 2 - 16_Diamond Pattern.cpp

---
+++ Assignments/Victor Li - Chapter 2 - 16_Diamond Pattern.cpp
@@ -0,0 +1,16 @@
+/*
+  Victor Li
+  Chapter 2: Diamond Pattern
+  This program outputs a diamond pattern into the console
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "   *\n" << "  ***\n" << " *****\n" << "*******\n" << " *****\n" << "  ***\n" << "   *\n";
+
+  return 0;
+}
