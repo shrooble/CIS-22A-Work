892d099ccd56824a95c3f8a42f3184d5723001de
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-16 20:43:15 -0800

    Update 1-15-2025 Classwork.cpp
    Indentation

--- Classwork/1-15-2025 Classwork.cpp
+++ Classwork/1-15-2025 Classwork.cpp
@@ -11,13 +11,13 @@ using namespace std;
 int main()
 {
     
-    // This code demonstrates floating point division
+// This code demonstrates floating point division
     cout << 13 / 5 << endl;
     cout << 91 / 7 << endl;
     cout << 13 / 5.0 << endl;
     cout << 91.0 / 7 << endl;
     
-    // This code outputs the Sun's distance and mass
+// This code outputs the Sun's distance and mass
     float distance;
     double mass;
     
@@ -27,27 +27,27 @@ int main()
     cout << "The Sun is " << distance << " meters away. \n";
     cout << "The Sun's mass is " << mass << " kilograms. \n\n";
     
-    // This code uses the sizeof() function
+// This code uses the sizeof() function
     double amount;
     cout << sizeof(double) << endl;
     cout << sizeof(amount) << endl;
     
-    // This code demonstrates boolean values
+// This code demonstrates boolean values
     bool value = true;
     
     cout << value << endl;
     value = false;
     cout << value << endl;
     
-    // This code demonstrates floating point variables
+// This code demonstrates floating point variables
     int rate = 2;
     float hours = 5.25;
-    //int pay;
+//int pay;
     float pay;
     pay = rate * hours;
     cout << "Your pay is " << pay << endl;
     
-    // This code asks the user their name and says hello back
+// This code asks the user their name and says hello back
     string userName; 
     
     cout << "\n What is your name?" << endl;
