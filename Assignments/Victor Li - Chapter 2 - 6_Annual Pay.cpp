443adb792fb20d876a54f727253f1a14aa856fa7
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-16 23:03:54 -0800

    Update Victor Li - Chapter 2 - 6_Annual Pay.cpp

--- Assignments/Victor Li - Chapter 2 - 6_Annual Pay.cpp
+++ Assignments/Victor Li - Chapter 2 - 6_Annual Pay.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 2: Annual Pay
-
+  This program calculates the total annual pay based on the amount for each pay period
 */
 
 #include <iostream>
@@ -10,7 +10,12 @@ using namespace std;
 
 int main()
 {
-  
+  double payAmount = 2200.0, payPeriods = 26, annualPay;
+
+//This calculates the annual pay by multiplying the pay amount by the pay periods in a year
+  annualPay = payAmount * payPeriods;
+
+  cout << "The total annual pay of the employee is " << annualPay << "." << endl;
 
   return 0;
 }
