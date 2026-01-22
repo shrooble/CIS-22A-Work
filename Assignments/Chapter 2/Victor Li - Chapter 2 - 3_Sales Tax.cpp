52a83b6e8402930f6bcf4fa9fe2dcb76fba41c6a
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-22 00:35:52 -0800

    Update Victor Li - Chapter 2 - 3_Sales Tax.cpp

--- Assignments/Chapter 2/Victor Li - Chapter 2 - 3_Sales Tax.cpp
+++ Assignments/Chapter 2/Victor Li - Chapter 2 - 3_Sales Tax.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 2: Sales Tax
-  This program
+  This program calculates and outputs the total sales tax on a purchase based on state and county sales tax
 */
 
 #include <iostream>
@@ -10,5 +10,12 @@ using namespace std;
 
 int main()
 {
-  //IDK what to put here
+  double purchase = 95.0, stateTax = 0.04, countyTax = 0.02, totalTax;
+
+// This finds the total tax of the purchase by adding the state and county taxes before multiplying them with the purchase amount
+  totalTax = purchase * (stateTax + countyTax);
+
+  cout << "The total sales tax on a $" << purchase << " purchase is: $" << totalTax << endl;
+
+  return 0;
 }
