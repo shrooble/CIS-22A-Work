9e63c26b9cbe7e85585d51099665743ed4004c83
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-27 23:35:08 -0800

    Create Victor Li - Chapter 3 - 10_How Much Insurance.cpp

---
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 10_How Much Insurance.cpp
@@ -0,0 +1,23 @@
+/*
+  Victor Li
+  Chapter 3: 10 - How Much Insurance?
+  This program calculates the minimum amount of insurance the property owner should buy based on their structure replacement cost
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double insurePercent = 0.80, structureCost, insuranceAmount;
+
+  cout << "How much would it cost to replace the property:\n";
+  cin >> structureCost;
+
+// This calculates the insurance amount by multiplying the structure cost by the insure percentage
+  insuranceAmount = structureCost * insurePercent;
+
+  cout << "The minimum amount of insurance you should get is: $" << insuranceAmount << endl;
+
+  return 0;
+}
