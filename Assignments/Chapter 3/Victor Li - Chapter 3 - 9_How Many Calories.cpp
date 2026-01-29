2db9bb866e8bd09de54af8dda12c2d92a527628d
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-29 01:13:41 -0800

    Update Victor Li - Chapter 3 - 9_How Many Calories.cpp

--- Assignments/Chapter 3/Victor Li - Chapter 3 - 9_How Many Calories.cpp
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 9_How Many Calories.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 3: 9 - How Many Calories?
-
+  This program calculates the amount of calories the user consumed from cookies based on their input
 */
 
 #include <iostream>
@@ -9,13 +9,16 @@
 using namespace std;
 
 int main() {
-  int cookieBag = 30, servingAmount = 10, servingCalories = 300;
-  double cookieAmount;
+  double cookieBag = 30.0, servingAmount = 10.0, servingCalories = 300.0, calories;
+  int cookieAmount;
 
   cout << "How many cookies did you eat:\n";
   cin >> cookieAmount;
 
-  
+// This calculates the calories consumed by dividing the serving calories by the serving amount and cookie bag amount (30) and multiplying the quotient by the cookies consumed
+  calories = ((servingCalories / servingAmount) / cookieBag) * cookieAmount;
+
+  cout << "Calories: " << calories << endl;
 
   return 0;
 }
