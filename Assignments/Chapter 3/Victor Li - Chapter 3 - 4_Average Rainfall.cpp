5c72953be34bab33517aabe9af19751119305187
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-29 18:33:26 -0800

    Update Victor Li - Chapter 3 - 4_Average Rainfall.cpp

--- Assignments/Chapter 3/Victor Li - Chapter 3 - 4_Average Rainfall.cpp
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 4_Average Rainfall.cpp
@@ -1,5 +1,7 @@
 /*
   Victor Li
+  Team Name: Cat++
+  Team Members: Kenny Nguyen
   Chapter 3: 4 - Average Rainfall
   This program calculates and outputs the average rainfall for 3 months
 */
@@ -14,14 +16,16 @@ int main() {
   string monthOne, monthTwo, monthThree;
   double rainOne, rainTwo, rainThree, averageRain;
 
-  cout << "Enter the month name followed by the amount of rainfall received (inches), each input should be separated by a space:\n";
-  cin >> monthOne >> rainOne >> monthTwo >> rainTwo >> monthThree >> rainThree;
+  cout << "Enter 3 different months, each input should be separated by a space:\n";
+  cin >> monthOne >> monthTwo >> monthThree;
+  cout << "Enter the amount of rainfall received (inches) for each month respectively, each input should be separated by a space:\n";
+  cin >> rainOne >> rainTwo >> rainThree;
 
-// This determines the average amount of rainfall by getting the sum of the rainfall amount for 3 months before dividing it by 3
+// This determines the average amount of rainfall by getting the sum of the rainfall amount for 3 months before dividing the sum by 3
   averageRain = (rainOne + rainTwo + rainThree) / 3;
   
-  cout << "The average rainfall for " << monthOne << ", " << monthTwo << ", and " << monthThree << endl;
-  cout << " is " << fixed << setprecision(2) << showpoint << averageRain << " inches.\n";
+  cout << "\nThe average rainfall for the months " << monthOne << ", " << monthTwo << ", and " << monthThree;
+  cout << " is: " << fixed << setprecision(3) << showpoint << averageRain << " inches\n";
 
   return 0;
 }
