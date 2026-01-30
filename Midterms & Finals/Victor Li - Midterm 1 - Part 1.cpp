b1689b1ec10f36191e5693048977cf7122d6ba43
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-29 21:20:17 -0800

    Create Victor Li - Midterm 1 - Part 1.cpp

---
+++ Midterms & Finals/Victor Li - Midterm 1 - Part 1.cpp
@@ -0,0 +1,17 @@
+/*
+  Victor Li
+  Team Name: Cat++
+  Team Members: Kenny Nguyen
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  /* 
+    use a for loop to keep the user in the menu until "end order"
+  */
+  return 0;
+}
