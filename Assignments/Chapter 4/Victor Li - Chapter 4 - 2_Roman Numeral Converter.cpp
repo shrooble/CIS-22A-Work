344a97f924fce7c540f5627abd9eb1a07f20e8ef
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-03 15:20:00 -0800

    Create Victor Li - Chapter 4 - 2_Roman Numeral Converter.cpp

---
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 2_Roman Numeral Converter.cpp
@@ -0,0 +1,20 @@
+/*
+  Victor Li
+  Chapter 4: Roman Numeral Converter
+
+*/
+
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+  int userNumber;
+
+  cout << "Enter a number for 1 - 10:\n";
+  cin >> userNumber;
+
+  switch(userNumber):
+
+  return 0;
+}
