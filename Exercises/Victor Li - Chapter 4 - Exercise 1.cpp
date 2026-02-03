04b0e81096dbb7ed8a668a5163af80468051dac2
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-03 14:01:30 -0800

    Update Victor Li - Chapter 4 - Exercise 1.cpp

--- Exercises/Victor Li - Chapter 4 - Exercise 1.cpp
+++ Exercises/Victor Li - Chapter 4 - Exercise 1.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main()
 {
   unsigned int seedValue;
-  double hours, hourlyRate, grossPay, avgPayHour, overtimeRate;
+  double hours, hourlyRate, grossPay, avgPayHour, overtimeRate, newHours;
   int documentNumber, overtimeHours;
   string name;
 
@@ -31,15 +31,15 @@ int main()
 
   if (hours > 40) {
 // If there is overtime pay, this calculates the remainder of hours / 40 so that overtimeHours has the overtime hours and hours has only the max 40 hours
-    overtimeHours = hours % 40;
-    hours = hours - overtimeHours;
+    overtimeHours = hours - 40;
+    newHours = hours - overtimeHours;
 // This calculates the gross pay by multiplying the rates with each overtime and regular hours and getting the sum of both 
-    grossPay = (hourlyRate * hours) + (overtimeRate * overtimeHours);
+    grossPay = (hourlyRate * newHours) + (overtimeRate * overtimeHours);
   }
     
   else {
 // If there is no overtime pay, this multiplies the hours worked by the hourly pay rate to get the gross pay
-  grossPay = hours * hourlyRate;
+    grossPay = hours * hourlyRate;
   }
 
 // This divides the gross pay by the hours worked to get the average pay per hour
@@ -56,10 +56,48 @@ int main()
   cout << "Company Name: " << name << endl;
   cout << "Hours Worked: " << hours << " hours" << endl;
   cout << "Hourly Pay Rate: $" << fixed << setprecision(2) << showpoint << hourlyRate << endl;
-  cout << "Overtime Pay Rate: $" << overtimeRate << endl;
+  cout << "Potential Overtime Pay Rate: $" << overtimeRate << endl;
   cout << "Document Number: #" << documentNumber << endl;
   cout << "Gross Pay: $" << grossPay << endl;
-  cout << "Average Hourly Pay (Calculated using type casting): $" << avgPayHour << endl;
+  cout << "Average Hourly Pay: $" << avgPayHour << endl;
 
   return 0;
 }
+
+/*
+Sample Output #1:
+  Enter your College or Company name:
+  De Anza
+  Enter the number of hours worked:
+  38
+  Enter the hourly pay rate:
+  10
+  
+  Information Summary
+  -------------------
+  Company Name: De Anza
+  Hours Worked: 38 hours
+  Hourly Pay Rate: $10.00
+  Potential Overtime Pay Rate: $15.00
+  Document Number: #1606
+  Gross Pay: $380.00
+  Average Hourly Pay: $10.00
+
+Sample Output #2:
+  Enter your College or Company name:
+  Google
+  Enter the number of hours worked:
+  50
+  Enter the hourly pay rate:
+  17
+  
+  Information Summary
+  -------------------
+  Company Name: Google
+  Hours Worked: 50 hours
+  Hourly Pay Rate: $17.00
+  Potential Overtime Pay Rate: $25.50
+  Document Number: #1246
+  Gross Pay: $935.00
+  Average Hourly Pay: $18.70
+*/
