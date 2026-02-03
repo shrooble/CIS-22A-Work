fd2e8429a5f90a9a207babbf8577e22ed081df3e
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-02 21:00:37 -0800

    Create Victor Li - Chapter 4 - 1_Minimum Maximum.cpp

---
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 1_Minimum Maximum.cpp
@@ -0,0 +1,28 @@
+/*
+  Victor Li
+  Chapter 4: Minimum/Maximum
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numberOne, numberTwo;
+
+  cout << "Enter 2 numbers, each separated by a space:\n";
+  cin >> numberOne >> numberTwo;
+
+  if (numberOne == numberTwo) {
+    cout << "\n" << numberOne << " is equal to " << numberTwo << endl;
+  }
+  else if (numberOne > numberTwo) {
+    cout << "\n" << numberOne << " is larger than " << numberTwo << endl;
+  }
+  else {
+        cout << "\n" << numberTwo << " is larger than " << numberOne << endl;
+  }
+
+  return 0;
+}
