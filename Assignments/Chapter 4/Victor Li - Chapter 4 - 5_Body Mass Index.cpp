9d18f5ad028d659c50f2b3d6f395705fb9e947c3
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-04 12:44:31 -0800

    Update Victor Li - Chapter 4 - 5_Body Mass Index.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 5_Body Mass Index.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 5_Body Mass Index.cpp
@@ -5,11 +5,32 @@
 */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
+  double BMI, weight, height;
 
+  cout << "Enter your weight:\n";
+  cin >> weight;
+  cout << "Enter your height:\n";
+  cin >> height;
+
+  BMI = (weight * 703) / pow(height, 2);
+
+  if (BMI < 0) {
+    cout << "\nInvalid Inputs\n";
+  }
+  else if (BMI < 18.5) {
+    cout << "\nBMI: " << BMI << ", underweight\n";
+  }
+  else if (BMI < 25) {
+    cout << "\nBMI: " << BMI << ", optimal\n";
+  }
+  else {
+    cout << "\nBMI: " << BMI << ", overweight\n";
+  }
 
   return 0;
 }
