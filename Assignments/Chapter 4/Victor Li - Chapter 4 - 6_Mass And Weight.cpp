61d85454c9d8b3c0c0ec65f03da24b20e2a54567
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-04 13:12:37 -0800

    Update Victor Li - Chapter 4 - 6_Mass And Weight.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 6_Mass And Weight.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 6_Mass And Weight.cpp
@@ -14,7 +14,17 @@ int main() {
   weight = mass * 9.8;
 
   if (weight < 10) {
-    cout << "Object Weight: " << weight << ", too light
+    cout << "\nObject Weight: " << weight << " N, Too Light\n";
+  }
+  else if (weight <= 1000) {
+    cout << "\nObject Weight: " << weight << " N\n";
+  }
+  else if (weight > 1000) {
+    cout << "\nObject Weight: " << weight << " N, Too Heavy\n";
+  }
+  else {
+    cout << "\nInvalid Inputs\n";
+  }
 
   
   return 0;
