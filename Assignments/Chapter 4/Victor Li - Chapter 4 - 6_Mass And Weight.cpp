de3e151a8b0310cce80464cf5c0a428028ca9fba
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-04 13:05:07 -0800

    Create Victor Li - Chapter 4 - 6_Mass And Weight.cpp

---
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 6_Mass And Weight.cpp
@@ -0,0 +1,21 @@
+/*
+  Victor Li
+  Chapter 4: 6 - Mass And Weight
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double mass, weight;
+
+  weight = mass * 9.8;
+
+  if (weight < 10) {
+    cout << "Object Weight: " << weight << ", too light
+
+  
+  return 0;
+}
