90cee0c0874a9c66189fe2aa792eb917df4f8765
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 14:40:54 -0800

    Update Victor Li - Chapter 4 - 4_Areas Of Rectangles.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 4_Areas Of Rectangles.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 4_Areas Of Rectangles.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 4: 4 - Areas of Rectangles
-
+  This program compares the area of 2 different rectangles
 */
 
 #include <iostream>
@@ -16,15 +16,20 @@ int main() {
   cout << "What is the length and width of the second rectangle, each input should be separated by a space:\n";
   cin >> lengthTwo >> widthTwo;
 
+// This finds the area of the 1st rectangle by the product of the first length and width inputs
   areaOne = lengthOne * widthOne;
+// This finds the area of the 2nd rectangle by the product of the second length and width inputs
   areaTwo = lengthTwo * widthTwo;
 
+// This checks whether the area of the 1st rectangle is larger than the 2nd rectangle
   if (areaOne > areaTwo) {
     cout << "\nThe area of rectangle one, " << areaOne << ", is greater than rectangle two, " << areaTwo << endl;
   }
+// This checks whether the area of the 1st rectangle is less than the 2nd rectangle
   else if (areaOne < areaTwo) {
     cout << "\nThe area of rectangle one, " << areaOne << ", is less than rectangle two, " << areaTwo << endl;
   }
+// This checks whether the area of the 1st rectangle is equal to the 2nd rectangle
   else if (areaOne == areaTwo) {
     cout << "\nThe area of rectangle one, " << areaOne << ", is equal to rectangle two, " << areaTwo << endl;
   }
