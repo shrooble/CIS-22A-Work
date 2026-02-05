058e8e91250b638edb875dea978bd1d7f2204eea
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 14:50:42 -0800

    Update Victor Li - Chapter 4 - 6_Mass And Weight.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 6_Mass And Weight.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 6_Mass And Weight.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 4: 6 - Mass And Weight
-
+  This program checks the weight of an object and outputs the weight and whether it is too light or too heavy
 */
 
 #include <iostream>
@@ -10,15 +10,19 @@ using namespace std;
 
 int main() {
   double mass, weight;
-
+  
+// This finds the weight by the product of the mass and 9.8
   weight = mass * 9.8;
 
+// This checks whether the weight is less than 10 N (Noninclusive)
   if (weight < 10) {
     cout << "\nObject Weight: " << weight << " N, Too Light\n";
   }
+// This checks whether the weight is less than 1000 N (Inclusive)
   else if (weight <= 1000) {
     cout << "\nObject Weight: " << weight << " N\n";
   }
+// This checks whether the weight is more than 1000 N (Noninclusive)
   else if (weight > 1000) {
     cout << "\nObject Weight: " << weight << " N, Too Heavy\n";
   }
