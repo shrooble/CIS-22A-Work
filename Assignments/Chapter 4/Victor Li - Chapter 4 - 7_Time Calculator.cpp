0205402a638eea81a722ed906085bbaf9abb7ab7
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 14:52:35 -0800

    Update Victor Li - Chapter 4 - 7_Time Calculator.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 7_Time Calculator.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 7_Time Calculator.cpp
@@ -14,13 +14,19 @@ int main() {
   cout << "Enter a time amount in seconds:\n";
   cin >> seconds;
 
+// This checks whether the amount is longer than 60 seconds (Inclusive)
   if (seconds >= 60) {
+  // 
     minutes = seconds / 60;
     cout << "\nThere are " << minutes << " minutes in that amount of time\n";
+  //
     if (seconds >= 3600) {
+    //
       hours = seconds / 3600;
       cout << "There are " << hours << " hours in that amount of time\n";
+    //
       if (seconds >= 86400) {
+      //
         days = seconds / 86400;
         cout << "There are " << days << " days in that amount of time\n";
       }
