0a7ae8c885af9c7c1eb5109fc96e1071ed6e76e2
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 14:45:56 -0800

    Update Victor Li - Chapter 4 - 5_Body Mass Index.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 5_Body Mass Index.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 5_Body Mass Index.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 4: 5 - Body Mass Index
-
+  This program checks the user's BMI and outputs whether they are underweight, optimal, or overweight considering their weight and height
 */
 
 #include <iostream>
@@ -17,17 +17,22 @@ int main() {
   cout << "Enter your height (inches):\n";
   cin >> height;
 
+// This calculates the BMI with the quotient between the product of the weight times 703, and the height squared
   BMI = (weight * 703) / pow(height, 2);
 
-  if (BMI < 0) {
+// This checks whether the BMI is a valid number (More than 0)
+  if (BMI <= 0) {
     cout << "\nInvalid Inputs\n";
   }
+// This checks whether the BMI is between 1 and 18.5 (Noninclusive)
   else if (BMI < 18.5) {
     cout << "\nBMI: " << BMI << ", underweight\n";
   }
-  else if (BMI < 25) {
+// This checks whether the BMI is between 18.5 and 25 (Inclusive)
+  else if (BMI <= 25) {
     cout << "\nBMI: " << BMI << ", optimal\n";
   }
+// Otherwise, if the BMI is larger than 25 it outputs "overweight"
   else {
     cout << "\nBMI: " << BMI << ", overweight\n";
   }
