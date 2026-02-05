b15d0980f6c220e7cb20183c411cf21d5cfc2537
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 14:38:27 -0800

    Update Victor Li - Chapter 4 - 3_Magic Dates.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 3_Magic Dates.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 3_Magic Dates.cpp
@@ -1,7 +1,7 @@
 /* 
   Victor Li
   Chapter 4: 3 - Magic Dates
-
+  This program checks whether the inputted date is a magic date
 */
 
 #include <iostream>
@@ -18,11 +18,14 @@ int main() {
   cout << "Enter a two-digit year:\n";
   cin >> year;
 
+// This multiplies the day and the month together
   magicDate = day * month;
-  
+
+// This checks whether the product of the month and day are equal to the year
   if (magicDate == year) {
     cout << "\n" << month << "/" << day << "/" << year << " is a magic date\n";
   }
+// Otherwise, it outputs that its not a magic date
   else if (magicDate != year) {
     cout << "\n" << month << "/" << day << "/" << year << " is not a magic date\n";
   }
