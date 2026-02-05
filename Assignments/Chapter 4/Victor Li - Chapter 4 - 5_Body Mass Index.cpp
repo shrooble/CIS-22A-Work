e23a96735cd8467443649e635ef5c6a927aedca2
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 14:51:11 -0800

    Update Victor Li - Chapter 4 - 5_Body Mass Index.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 5_Body Mass Index.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 5_Body Mass Index.cpp
@@ -20,7 +20,7 @@ int main() {
 // This calculates the BMI with the quotient between the product of the weight times 703, and the height squared
   BMI = (weight * 703) / pow(height, 2);
 
-// This checks whether the BMI is a valid number (More than 0)
+// This checks whether the BMI is a valid number (More than 0) (Inclusive)
   if (BMI <= 0) {
     cout << "\nInvalid Inputs\n";
   }
