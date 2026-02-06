18a979ace530569a36bcd89d2a3dca6e3e97050d
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 20:32:55 -0800

    Update Victor Li - Chapter 4 - 7_Time Calculator.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 7_Time Calculator.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 7_Time Calculator.cpp
@@ -9,31 +9,39 @@
 using namespace std;
 
 int main() {
-  double seconds, minutes, hours, days;
+  int seconds, minutes, hours, days;
 
   cout << "Enter a time amount in seconds:\n";
   cin >> seconds;
 
-// This checks whether the amount is longer than 60 seconds (Inclusive)
-  if (seconds >= 60) {
-  // This divides the seconds by 60 to get the amount of minutes
-    minutes = seconds / 60;
-    cout << "\nThere are " << minutes << " minutes in that amount of time\n";
-  // This checks whether the amount is longer than 3600 seconds (Inclusive), after checking whether it was longer than 60 seconds
-    if (seconds >= 3600) {
-    // This divides the seconds by 3600 to get the amount of hours
-      hours = seconds / 3600;
-      cout << "There are " << hours << " hours in that amount of time\n";
-    // This checks whether the amount is longer than 86,400 seconds (Inclusive), after checking whether it was longer than 3600 seconds
-      if (seconds >= 86400) {
-      // This divides the seconds by 86,400 to get the amount of days
-        days = seconds / 86400;
-        cout << "There are " << days << " days in that amount of time\n";
+// This checks whether seconds is a valid input (more than 0 seconds, Noninclusive)
+  if  (seconds > 0) {
+  // This checks whether the amount is longer than 60 seconds (Inclusive)
+    if (seconds >= 60) {
+    // This divides the seconds by 60 to get the amount of minutes
+      minutes = seconds / 60;
+      cout << "\nThere are " << minutes << " minutes in that amount of time\n";
+    // This checks whether the amount is longer than 3600 seconds (Inclusive), after checking whether it was longer than 60 seconds
+      if (seconds >= 3600) {
+      // This divides the seconds by 3600 to get the amount of hours
+        hours = seconds / 3600;
+        cout << "\nThere are " << hours << " hours in that amount of time\n";
+      // This checks whether the amount is longer than 86,400 seconds (Inclusive), after checking whether it was longer than 3600 seconds
+        if (seconds >= 86400) {
+        // This divides the seconds by 86,400 to get the amount of days
+          days = seconds / 86400;
+          cout << "\nThere are " << days << " days in that amount of time\n";
+        }
       }
     }
+  // This only occurs if (seconds > 0) && (seconds < 60)
+    else {
+      cout << "\n" << seconds << " seconds is less than 1 minute\n";
+    }
   }
+// This only occurs if seconds <= 0
   else {
-    cout << "Invalid Input\n";
+    cout << "\nInvalid Input\n";
   }
 
   return 0;
