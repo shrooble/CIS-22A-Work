6d2725ea40adc0950d9da58abade60bc3e818255
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 21:39:13 -0800

    Update Victor Li - Chapter 4 - 9_Change For a Dollar Game.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 9_Change For a Dollar Game.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 9_Change For a Dollar Game.cpp
@@ -1,15 +1,43 @@
 /*
   Victor Li
   Chapter 4: 9 - Change for a Dollar Game
-
+  This program asks the user to add up to $1 using a user inputted amount of pennies, nickels, dimes, and quarters
 */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-  
+  double pennies, nickels, dimes, quarters, total;
+
+  cout << "Enter the number of pennies, nickels, dimes, and quarters (In this order, separated by spaces) to add up to 1 dollar:\n" << fixed << setprecision(2) << showpoint;
+  cin >> pennies >> nickels >> dimes >> quarters;
+
+// This multiplies each coin with their respective coin values
+  nickels *= 5.0;
+  dimes *= 10.0;
+  quarters *= 25.0;
+
+// This adds the sum of each coin before dividing by 100 
+  total = (pennies +  nickels + dimes + quarters) / 100;
+
+// This checks whether the coins add up to $1 exactly
+  if (total == 1) {
+    cout << "\nYou correctly got to $1 using: " << pennies << " pennies, " << nickels / 5 << " nickels, " << dimes / 10 << " dimes, " << quarters / 25 << " and quarters\n";
+  }
+// This checks whether the coins were less than $1
+  else if (total < 1) {
+    cout << "\nYou were $" << 1 - total << " short of $1\n";
+  }
+// This checks whether the coins were more than $1
+  else if (total > 1) {
+    cout << "\nYou were $" << total - 1 << " more than $1\n";
+  }
+  else {
+    cout << "\nInvalid Inputs\n";
+  }
 
   return 0;
 }
