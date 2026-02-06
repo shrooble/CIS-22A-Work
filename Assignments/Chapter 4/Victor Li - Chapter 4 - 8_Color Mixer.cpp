4bb7b55c02324c7db41bcc61ee45e2e359640642
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 21:16:28 -0800

    Update Victor Li - Chapter 4 - 8_Color Mixer.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 8_Color Mixer.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 8_Color Mixer.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 4: 8 - Color Mixer
-
+  This program outputs the relevant primary color combination between Red, Blue, or Yellow based on the user's input of 2 primary colors
 */
 
 #include <iostream>
@@ -15,17 +15,21 @@ int main() {
   cout << "Enter two primary colors to mix (Red, Blue, or Yellow), each input should be separated by a space:\n";
   cin >> colorOne, colorTwo;
 
+// This checks whether the inputs are Red or Blue
   if ((colorOne == "Red" && colorTwo == "Blue") || (colorOne == "Blue" &&  colorTwo == "Red")) {
     cout << "\n" << colorOne << " and " << colorTwo << " mixed together create purple\n";
   }
+// This checks whether the inputs are Red or Yellow
   else if ((colorOne == "Red" && colorTwo == "Yellow") || (colorOne == "Yellow" &&  colorTwo == "Red")) {
     cout << "\n" << colorOne << " and " << colorTwo << " mixed together create orange\n";
   }
+// This checks whether the inputs are Blue or Yellow
   else if ((colorOne == "Blue" && colorTwo == "Yellow") || (colorOne == "Yellow" &&  colorTwo == "Blue")) {
     cout << "\n" << colorOne << " and " << colorTwo << " mixed together create green\n";
   }
+// This occurs if the input is something other than Red, Blue, or Yellow
   else {
-    cout << "Invalid Inputs";
+    cout << "\nInvalid Inputs\n";
   }
 
   return 0;
