aa5f9f65a6ff9303dd7830221d3a2ee7c0f7717d
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-05 21:34:52 -0800

    Update Victor Li - Chapter 4 - 10_Days In a Month.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 10_Days In a Month.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 10_Days In a Month.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 4: 10 - Days In a Month
-
+  This program outputs the amount of days for a user inputted a month, accounting for potential leap years
 */
 
 #include <iostream>
@@ -13,17 +13,21 @@ int main() {
 
   cout << "Enter a month from 1 - 12:\n";
   cin >> month;
-  cout << "Enter a year:\n";
+  cout << "Enter a four-digit year:\n";
   cin >> year;
 
+// This checks whether the year is divisible by 100 (True if it has no remainder)
   if ( !(year % 100)) {
+  // This checks whether the year is divisible by 400 (True if it has no remainder), after checking if it was divisible by 100 
     if ( !(year % 400)) {
       leapYear = 1;
     }
+  // This only occurs if the year isn't divisible by 400 (If it had a remainder)
     else {
       leapYear = 0;
     }
   }
+// This checks whether the year is divisible by 4 (True if it has no remainder), failing the divisible by 100 check
   else if ( !(year % 4)) {
     leapYear = 1;
   }
@@ -31,54 +35,57 @@ int main() {
     leapYear = 0;
   }
 
+// This switch statement checks for a month between 1 and 12 and outputs the days for that month, otherwise it outputs "Invalid Month Input"
   switch (month) {
     case 0:
       cout << "\nInvalid Month Input\n";
       break;
     case 1:
-      cout << "\nThere are 31 days\n";
+      cout << "\nThere are 31 days in that month\n";
       break;
     case 2:
+    // This checks whether it is a leap year or not for February
       if (leapYear) {
-        cout << "\nThere are 29 days\n";
+        cout << "\nThere are 29 days in that month\n";
       }
       else {
-        cout << "\nThere are 28 days\n";
+        cout << "\nThere are 28 days in that month\n";
       }
       break;
     case 3:
-      cout << "\nThere are 31 days\n";
+      cout << "\nThere are 31 days in that month\n";
       break;
     case 4:
-      cout << "\nThere are 30 days\n";
+      cout << "\nThere are 30 days in that month\n";
       break;
     case 5:
-      cout << "\nThere are 31 days\n";
+      cout << "\nThere are 31 days in that month\n";
       break;
     case 6:
-      cout << "\nThere are 30 days\n";
+      cout << "\nThere are 30 days in that month\n";
       break;
     case 7:
-      cout << "\nThere are 31 days\n";
+      cout << "\nThere are 31 days in that month\n";
       break;
     case 8:
-      cout << "\nThere are 31 days\n";
+      cout << "\nThere are 31 days in that month\n";
       break;
     case 9:
-      cout << "\nThere are 30 days\n";
+      cout << "\nThere are 30 days in that month\n";
       break;
     case 10:
-      cout << "\nThere are 31 days\n";
+      cout << "\nThere are 31 days in that month\n";
       break;
     case 11:
-      cout << "\nThere are 30 days\n";
+      cout << "\nThere are 30 days in that month\n";
       break;
     case 12:
-      cout << "\nThere are 31 days\n";
+      cout << "\nThere are 31 days in that month\n";
       break;
     default:
-      cout << "\nInvalid Inputs\n";
+      cout << "\nInvalid Month Input\n";
       break;
+  }
 
   return 0;
 }
