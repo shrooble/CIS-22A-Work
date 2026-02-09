14ca92453a0036284b864f097a77a160719b8037
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-08 18:20:18 -0800

    Update Victor Li - Chapter 4 - 8_Color Mixer.cpp

--- Assignments/Chapter 4/Victor Li - Chapter 4 - 8_Color Mixer.cpp
+++ Assignments/Chapter 4/Victor Li - Chapter 4 - 8_Color Mixer.cpp
@@ -1,11 +1,12 @@
 /*
   Victor Li
   Chapter 4: 8 - Color Mixer
-  This program outputs the relevant primary color combination between Red, Blue, or Yellow based on the user's input of 2 primary colors
+  This program outputs the relevant primary color combination between red, blue, or yellow based on the user's input of 2 primary colors
 */
 
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -13,21 +14,25 @@ int main() {
   string colorOne, colorTwo;
 
   cout << "Enter two primary colors to mix (Red, Blue, or Yellow), each input should be separated by a space:\n";
-  cin >> colorOne, colorTwo;
+  cin >> colorOne >> colorTwo;
 
-// This checks whether the inputs are Red or Blue
-  if ((colorOne == "Red" && colorTwo == "Blue") || (colorOne == "Blue" &&  colorTwo == "Red")) {
+// This forces the 1st letter to become lowercase, so it doesn't matter if the input is "Red" or "red"
+  colorOne.at(0) = tolower(colorOne.at(0));
+  colorTwo.at(0) = tolower(colorTwo.at(0));
+
+// This checks whether the inputs are red or blue
+  if ((colorOne == "red" && colorTwo == "blue") || (colorOne == "blue" &&  colorTwo == "red")) {
     cout << "\n" << colorOne << " and " << colorTwo << " mixed together create purple\n";
   }
-// This checks whether the inputs are Red or Yellow
-  else if ((colorOne == "Red" && colorTwo == "Yellow") || (colorOne == "Yellow" &&  colorTwo == "Red")) {
+// This checks whether the inputs are red or yellow
+  else if ((colorOne == "red" && colorTwo == "yellow") || (colorOne == "yellow" &&  colorTwo == "red")) {
     cout << "\n" << colorOne << " and " << colorTwo << " mixed together create orange\n";
   }
-// This checks whether the inputs are Blue or Yellow
-  else if ((colorOne == "Blue" && colorTwo == "Yellow") || (colorOne == "Yellow" &&  colorTwo == "Blue")) {
+// This checks whether the inputs are blue or yellow
+  else if ((colorOne == "blue" && colorTwo == "yellow") || (colorOne == "yellow" &&  colorTwo == "blue")) {
     cout << "\n" << colorOne << " and " << colorTwo << " mixed together create green\n";
   }
-// This occurs if the input is something other than Red, Blue, or Yellow
+// This occurs if the input is something other than red, blue, or yellow
   else {
     cout << "\nInvalid Inputs\n";
   }
