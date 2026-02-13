a4e37300c522f64b5ce900b151dd9b04f5965995
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-12 21:05:28 -0800

    Update Victor Li - Revised Midterm 1 - Part 1.cpp

--- Midterms & Finals/Victor Li - Revised Midterm 1 - Part 1.cpp
+++ Midterms & Finals/Victor Li - Revised Midterm 1 - Part 1.cpp
@@ -1,1 +1,19 @@
+/*
+  Victor Li
+  Team Name: Cat++
+  Team Members: Kenny Nguyen
+  This program provides a menu for 5 burgers from the De Anza College Food Court and outputs a formatted bill to both the console and an output file
+*/
 
+#include <iostream> 
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string burgerOne = "De Anza Burger", burgerTwo = "Bacon Cheese Burger", burgerThree = "Mushroom Swiss Burger", burgerFour = "Western Burger", 
+          burgerFive = "Don Cali Burger", taxOption;
+  
+  return 0;
+}
