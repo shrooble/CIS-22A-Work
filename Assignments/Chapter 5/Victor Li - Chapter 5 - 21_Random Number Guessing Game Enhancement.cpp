569153b246645238f66dd9dd4300e247e37096e2
Victor <113949103+shrooble@users.noreply.github.com>
2026-02-18 12:13:11 -0800

    Create Victor Li - Chapter 5 - 21_Random Number Guessing Game Enhancement.cpp

---
+++ Assignments/Chapter 5/Victor Li - Chapter 5 - 21_Random Number Guessing Game Enhancement.cpp
@@ -0,0 +1,48 @@
+
+/*
+  Victor Li
+  Chapter 5: 20 - Random Number Guessing Game
+  This program generates a random number and asks the user to guess what the number is
+*/
+
+#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+  int randomNumber, userGuess, counter;
+
+// This randomizes and assigns the random number based on the user's time
+  srand(time(0));
+  randomNumber = rand() % 1000 + 1;
+
+// This loop continues until the user guesses the number
+  while (userGuess != randomNumber) {
+  // This loop checks whether the user correctly input a valid number from the range 1 - 1000
+    while (userGuess > 1000 || userGuess < 1) {
+      cout << "Guess a random number from 1 - 1000:\n";
+      cin >> userGuess;  
+      counter += 1;
+    }
+    if (userGuess > randomNumber) {
+      cout << "Too high, try again.\n";
+      cin >> userGuess;  
+      counter += 1;
+    }
+    else if (userGuess < randomNumber) {
+      cout << "Too low, try again.\n";
+      cin >> userGuess;  
+      counter += 1;
+    }
+    else {
+      cout << "Error\n";
+    }
+  }
+
+  cout << "You guessed the correct number: " << randomNumber << endl;
+  cout << "Using " << counter << " tries.\n";
+  
+  return 0;
+}
