13059dca646639224a160c5580b4fedff053c708
Victor <113949103+shrooble@users.noreply.github.com>
2026-03-12 13:52:52 -0700

    Update Victor Li - Chapter 5 - 16_Savings Account Balance.cpp

--- Assignments/Chapter 5/Victor Li - Chapter 5 - 16_Savings Account Balance.cpp
+++ Assignments/Chapter 5/Victor Li - Chapter 5 - 16_Savings Account Balance.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 
 int main() {
-  double annualInterestRate = 0.0, monthlyInterestRate, balance, deposit, withdraws, monthlyInterest, totalInterest;
-  int months, depositCount = 0, withdrawCount = 0;
+  double annualInterestRate = 0.0, monthlyInterestRate = 0.0, balance, deposit = 0.0, withdraws = 0.0, monthlyInterest, totalInterest = 0.0;
+  int months, depositCount = 0, withdrawCount = 0, monthCount = 0;
   bool positiveDeposit = false, positiveWithdraw = false;
 
   cout << "Enter your starting balance:\n";
@@ -29,18 +29,24 @@ int main() {
 
 // This loops for the amount of months the user inputted
   for (int i = 1; i <= months; ++i) {
+    positiveDeposit = false;
+    positiveWithdraw = false;
+    
   // This checks whether the balance is above 0 at any point
     while (balance >= 0) {
     // This loops ensures the user enters a deposit amount above 0
       while (!positiveDeposit) {
-        cout << "Enter a positive amount deposited into the account for month: " << i << endl;
+        cout << "\nEnter a positive amount deposited into the account for month: " << i << endl;
         cout << "(Enter a 0 if there were no deposits)\n";
         cin >> deposit;
         if (deposit < 0) {
           cout << " - Enter a positive amount\n";
         }
+        else if (deposit == 0) {
+          positiveDeposit = true;
+        }
         else {
-        // This adds the deposit to the balance, counts the amount of deposits, and exits the loop
+        // This adds the deposit to the balance, counts the amount of deposits, and exits the deposit loop
           balance += deposit;
           depositCount += 1;
           positiveDeposit = true;
@@ -49,20 +55,25 @@ int main() {
       
     // This loops ensures the user enters a withdrawal amount above 0
       while (!positiveWithdraw) {
-        cout << "Enter the amount withdrawn from the account for month: " << i << endl;
+        cout << "\nEnter the amount withdrawn from the account for month: " << i << endl;
         cout << "(Enter a 0 if there were no withdraws)\n";
         cin >> withdraws;
         if (withdraws < 0) {
           cout << " - Enter a positive withdraw amount\n";
         }
+        else if (withdraws == 0) {
+          positiveWithdraw = true;
+        }
         else {
-      // This subtracts the withdrawal from the balance, counts the amount of withdrawals, and exits the loop
+      // This subtracts the withdrawal from the balance, counts the amount of withdrawals, and exits the withdraw loop
           balance -= withdraws;
           withdrawCount += 1;
           positiveWithdraw = true;
         }
       }
-
+      
+      monthCount = i;
+      
     // This calculates the monthly interest by the product of the monthly interest rate with the balance, also adding the monthly interest to the total interest 
       if (balance > 0) {
         monthlyInterest = balance * monthlyInterestRate;
@@ -73,16 +84,17 @@ int main() {
     // This adds the monthly interest to the balance and total interest
       totalInterest += monthlyInterest;
       balance += monthlyInterest;
+      break;
     }
     
   // This occurs only if the balance goes negative
     if (balance < 0) {
-      cout << "The account has been closed on month " << i << " for a negative balance of " << balance << endl;
+      cout << "The account has been closed on month " << monthCount << " for a negative balance of " << balance << endl;
       break;
     }
   }
   
-  cout << "Ending Balance: " << balance << endl;
+  cout << "\nEnding Balance: " << balance << endl;
   cout << "Total Deposits: " << depositCount << endl;
   cout << "Total Withdraws: " << withdrawCount << endl;
   cout << "Total Interest: " << totalInterest << endl;
