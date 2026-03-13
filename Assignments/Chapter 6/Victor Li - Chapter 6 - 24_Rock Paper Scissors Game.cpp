c09b4ca73ba687d22a192bebb8171ae397622b9a
Victor <113949103+shrooble@users.noreply.github.com>
2026-03-12 20:47:13 -0700

    Create Victor Li - Chapter 6 - 24_Rock Paper Scissors Game.cpp

---
+++ Assignments/Chapter 6/Victor Li - Chapter 6 - 24_Rock Paper Scissors Game.cpp
@@ -0,0 +1,22 @@
+/*
+  Victor Li
+  Chapter 6: 24 - Rock, Paper, Scissors, Game
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+  int userChoice;
+  
+
+  switch (userChoice) {
+    case "rock":
+    case "Rock":
+      
+  return 0;
+}
